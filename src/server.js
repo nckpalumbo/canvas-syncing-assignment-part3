0d5bdf4c4a7d4cc6f2e5533778a1a20053b721ca
Nick <nap2440@rit.edu>
2018-02-12 12:33:56 -0500

    Add files via upload
    fix error

--- src/server.js
+++ src/server.js
@@ -23,29 +23,29 @@ const io = socketio(app);
 app.listen(PORT);
 
 io.on('connection', (socket) => {
-  //socket.join('room1');
-    
+  // socket.join('room1');
+
   socket.on('join', (data) => {
-     console.log("Socket: " + data.user + " has joined.");
-     socket.join('room1');
+    console.log(`Socket: ${data.user} has joined.`);
+    socket.join('room1');
   });
 
   socket.on('draw', (data) => {
-      io.sockets.in('room1').emit('draw', { user: data.user, circle: data.circle });
-      ///socket.to('room1').emit('draw', {user: data.user, circle: data.circle});
+    io.sockets.in('room1').emit('draw', { user: data.user, circle: data.circle });
+    // /socket.to('room1').emit('draw', {user: data.user, circle: data.circle});
   });
-  
+
   socket.on('leaving', (data) => {
-      console.log("Socket: " + data.user + " left.");
-      socket.leave('room1');
-      
-      io.sockets.in('room1').emit('leave', data.user); 
+    console.log(`Socket: ${data.user} left.`);
+    socket.leave('room1');
+
+    io.sockets.in('room1').emit('leave', data.user);
   });
-    
-  socket.on('disconnect', (data) => {
-      //socket.leave('room1');
-      
-      console.log("User Disconnected...");
+
+  socket.on('disconnect', () => {
+    // socket.leave('room1');
+
+    console.log('User Disconnected...');
   });
 });
 
